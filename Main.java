76c74fb7ba432e8b497135730e2c04153a946b2c
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-04-06 08:55:41 +0200

    a

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Utworzenie obiektu klasy Point z inicjalizacją wartościami
+        Point point = new Point(3.5, 4.5);
+
+        // Wyświetlenie współrzędnych punktu używając metod getX i getY
+        System.out.println("Punkt ma współrzędne: x = " + point.getX() + ", y = " + point.getY());
+    }
+}
+
